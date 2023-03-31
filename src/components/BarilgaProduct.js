a510fa10f906cb45c08036879e5ad3384ad75e60
Umirbyek <umirbyek.s@monosolution.co>
2023-03-31 10:40:59 +0800

    image upload

--- src/components/BarilgaProduct.js
+++ src/components/BarilgaProduct.js
@@ -1,52 +1,54 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import style from "./BarilgaProduct.module.css";
 const BarilgaProduct = () => {
-  const {productId}=useParams()
-  const CompanyData=[{image:`GOLD-LOGO.png`,title:"БЕТОН ӨНГӨЛӨГЧ",id:1},{image:"logo-blue(1).png",title:"БЛОКНЫ МАШИН",id:1},{image:"logo-orenga.png",title:"ЦАХИЛГААНЫ ХЭРЭГСЭЛ",id:1},{image:"muuun.png",title:"КАРКАЗ МУЖААНЫ БАГАЖ",id:1},{image:`GOLD-LOGO.png`,title:"БЕТОН ӨНГӨЛӨГЧ",id:1},{image:"logo-blue(1).png",title:"БЛОКНЫ МАШИН",id:1},{image:"logo-orenga.png",title:"ЦАХИЛГААНЫ ХЭРЭГСЭЛ",id:1},{image:"muuun.png",title:"КАРКАЗ МУЖААНЫ БАГАЖ",id:1}]
+  const { productId } = useParams();
+  const CompanyData = [
+    { image: "betonMashin.png", title: "БЕТОН ӨНГӨЛӨГЧ", id: 1 },
+    { image: "blockMashin.png", title: "БЛОКНЫ МАШИН", id: 1 },
+    { image: "heregselMashin.png", title: "ЦАХИЛГААНЫ ХЭРЭГСЭЛ", id: 1 },
+    { image: "karkazMashin.png", title: "КАРКАЗ МУЖААНЫ БАГАЖ", id: 1 },
+    { image: `betonMashin.png`, title: "БЕТОН ӨНГӨЛӨГЧ", id: 1 },
+    { image: "blockMashin.png", title: "БЛОКНЫ МАШИН", id: 1 },
+    { image: "heregselMashin.png", title: "ЦАХИЛГААНЫ ХЭРЭГСЭЛ", id: 1 },
+    { image: "karkazMashin.png", title: "КАРКАЗ МУЖААНЫ БАГАЖ", id: 1 },
+  ];
 
-  console.log(productId)
-    const routePath=useLocation();
-    console.log(routePath)
-    const handleClick=()=>{
-      
-    }
+  console.log(productId);
+  const routePath = useLocation();
+  console.log(routePath);
+  const handleClick = () => {};
   return (
-    <div className={style.Container}  onClick={handleClick}>
-      <div className={style.TextBox} >
-      <div className={style.Text}>
-      <h1>Онцлох бүтээгдэхүүн</h1>
-   
-      </div>
-      <div className={style.TextHr}>
-    <div className={style.Hr}></div>
-      </div>
-  
-
+    <div className={style.Container} onClick={handleClick}>
+      <div className={style.TextBox}>
+        <div className={style.Text}>
+          <h1>Онцлох бүтээгдэхүүн</h1>
+        </div>
+        <div className={style.TextHr}>
+          <div className={style.Hr}></div>
+        </div>
       </div>
       <div className={style.ImContainer}>
-    {
-      CompanyData.filter(data=> data.id ==productId).map(datas=>{
-        return(
-          <div className={style.ProductContainer}>
-            <div className={style.TextImageCont}>
-            <div className={style.HeaderImage}><span>Зураг</span></div>
-            <div className={style.Title}><span>{datas.title}</span></div>
+        {CompanyData.filter((data) => (data.id = productId)).map((datas) => {
+          return (
+            <div className={style.ProductContainer}>
+              <div className={style.TextImageCont}>
+                <div className={style.HeaderImage}>
+                  <span>
+                    <img src={require(`../img/${datas.image}`)} alt=""  style={{ height: "20", width: "50" }}/>
+                  </span>
+                </div>
+                <div className={style.Title}>
+                  <span>{datas.title}</span>
+                </div>
+              </div>
             </div>
-          
-          
-          </div>
-        )
-      }
-      
-
-        
-      )
-    }
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BarilgaProduct
+export default BarilgaProduct;
--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,96 +1,144 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import style from "./Home.module.css";
-import { Link, useNavigate } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import {Swiper,SwiperSlide}from "swiper/react";
-import { Autoplay,Pagination,Navigation } from 'swiper';
+import { Link, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 const Home = () => {
-    const location=useLocation()
-   const BackData=[{image:"1"},{image:"2"},{image:"2"},{image:"2"},{image:"2"}]
-    const imageData=[{image:`GOLD-LOGO.png`,title:"ХӨРӨНГӨ ОРУУЛАЛТ"},{image:"logo-blue(1).png",title:"БАРИЛГА ТОНОГ ТӨХӨӨРӨМЖ ХУДАЛДАА"},{image:"logo-orenga.png",title:"БАРИЛГА МАТЕРИАЛ ХУДАЛДАА"},{image:"muuun.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"},{image:"logo-hoh.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"}]
-    const CompanyData=[{image:`GOLD-LOGO.png`,title:"ХӨРӨНГӨ ОРУУЛАЛТ",id:1},{image:"logo-blue(1).png",title:"БАРИЛГА ТОНОГ ТӨХӨӨРӨМЖ ХУДАЛДАА",id:1},{image:"logo-orenga.png",title:"БАРИЛГА МАТЕРИАЛ ХУДАЛДАА",id:1},{image:"muuun.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ",id:1},{image:"logo-hoh.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"},{image:"logo-hoh.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"},{image:"logo-hoh.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"},{image:"logo-hoh.png",title:"ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ"}]
-    const navigate=useNavigate();  
-    // <Link to={`/home/buteegdehuun/${data.id}`}>Дэлгэрэнгүй</Link> 
-    const handleClick=(id,pathname)=>{
-        console.log(id)
-        console.log(location.pathname)
-        navigate(`/home/buteegdehuun/${id}`)
-
-
-    }
-    return (
+  const location = useLocation();
+  const BackData = [
+    { image: "1" },
+    { image: "2" },
+    { image: "2" },
+    { image: "2" },
+    { image: "2" },
+  ];
+  const imageData = [
+    { image: `GOLD-LOGO.png`, title: "ХӨРӨНГӨ ОРУУЛАЛТ" },
+    { image: "logo-blue(1).png", title: "БАРИЛГА ТОНОГ ТӨХӨӨРӨМЖ ХУДАЛДАА" },
+    { image: "logo-orenga.png", title: "БАРИЛГА МАТЕРИАЛ ХУДАЛДАА" },
+    { image: "muuun.png", title: "ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ" },
+    { image: "logo-hoh.png", title: "УУЛ УУРХАЙН БҮТЭЭГДЭХҮҮН ХУДАЛДАА" },
+  ];
+  const CompanyData = [
+    { image: `GOLD-LOGO.png`, title: "ХӨРӨНГӨ ОРУУЛАЛТ", id: 1 },
+    {
+      image: "logo-blue(1).png",
+      title: "БАРИЛГА ТОНОГ ТӨХӨӨРӨМЖ ХУДАЛДАА",
+      id: 1,
+    },
+    { image: "logo-orenga.png", title: "БАРИЛГА МАТЕРИАЛ ХУДАЛДАА", id: 1 },
+    { image: "muuun.png", title: "ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ", id: 1 },
+    { image: "logo-hoh.png", title: "ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ" },
+    { image: "logo-hoh.png", title: "ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ" },
+    { image: "logo-hoh.png", title: "ЗУУЧ ХАУС ГАЗАР ХӨРӨНГӨ" },
+    { image: "logo-hoh.png", title: "УУЛ УУРХАЙН БҮТЭЭГДЭХҮҮН ХУДАЛДАА" },
+  ];
+  const navigate = useNavigate();
+  // <Link to={`/home/buteegdehuun/${data.id}`}>Дэлгэрэнгүй</Link>
+  const handleClick = (id, pathname) => {
+    console.log(id);
+    console.log(location.pathname);
+    navigate(`/home/buteegdehuun/${id}`);
+  };
+  return (
     <div className={style.Container}>
-    <div className={style.TextContainer}>
-    <div className={style.Text}><h1>ХАМТЫН ХҮЧ</h1></div>
-      <div className={style.Text}><h1>АМЖИЛТЫН ТҮЛХҮҮР</h1></div>
-      <div className={style.Text}><hr style={{background: 'yellow',color: 'yellow',borderColor: 'yellow',borderRadius:"5px", height: '4px', width:"30%",marginTop:"10px"}}/></div>
-      <div className={style.SpanContainer}>
-      <span className={style.SpanText}>Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore e Uplore magna. Quis ipsum suspendklfjslfkdsljfklsdjgdfjhgkjhkdisse ultrices gravida</span>
-      </div>
-      
+      <div className={style.TextContainer}>
+        <div className={style.Text}>
+          <h1>ХАМТЫН ХҮЧ</h1>
+        </div>
+        <div className={style.Text}>
+          <h1>АМЖИЛТЫН ТҮЛХҮҮР</h1>
+        </div>
+        <div className={style.Text}>
+          <hr
+            style={{
+              background: "yellow",
+              color: "yellow",
+              borderColor: "yellow",
+              borderRadius: "5px",
+              height: "4px",
+              width: "30%",
+              marginTop: "10px",
+            }}
+          />
+        </div>
+        <div className={style.SpanContainer}>
+          <span className={style.SpanText}>
+            Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do
+            eiusmod tempor incididunt ut labore e Uplore magna. Quis ipsum
+            suspendklfjslfkdsljfklsdjgdfjhgkjhkdisse ultrices gravida
+          </span>
+        </div>
       </div>
       <div className={style.LogoContainer}>
-      {
-        imageData && imageData.map((data)=>{
-            return(
-                <div className={style.Logo}><img src={ require(`../../img/${data.image}`) } style={{height:"20",width:"50"}} alt=""/>
-                <div className={style.ImageText}><span>{data.title}</span></div>
-                
+        {imageData &&
+          imageData.map((data) => {
+            return (
+              <div className={style.Logo}>
+                <img
+                  src={require(`../../img/${data.image}`)}
+                  style={{ height: "20", width: "50" }}
+                  alt=""
+                />
+                <div className={style.ImageText}>
+                  <span>{data.title}</span>
                 </div>
-            )
-        })
-    }
-      
+              </div>
+            );
+          })}
       </div>
 
-  
-    <div className={style.ImContainer}>
-
-        {
-            
-          CompanyData &&  CompanyData.map((data)=>{
+      <div className={style.ImContainer}>
+        {CompanyData &&
+          CompanyData.map((data) => {
             return (
-                <div className={style.SectionContainer}>
-                    <div className={style.SectionImageContainer}>
-                        <div className={style.SectionImage}></div>
-                    
-                    </div>
-                    <div className={style.SectionIconAndText}>
-                        <div className={style.SectionIconAndbtn}>
-                        <div className={style.SectionImageIcon}>
-                        <img src={require(`../../img/${data.image}`)} style={{display:"flex",alignItems:"center",width:"100%",height:"100%"}}/>
-                        </div>
-                        
-                        <div className={style.SectionTitle}>
-                        <div className={style.Text}><span>{data.title}</span></div>
-                        <div className={style.Btn}> <button onClick={()=>{handleClick(data.id)}}>Дэлгэрэнгүй</button></div>
-                        
-                     
-                       
-                        </div>
-                        
-                        </div>
-                    </div>
+              <div className={style.SectionContainer}>
+                <div className={style.SectionImageContainer}>
+                  <div className={style.SectionImage}></div>
                 </div>
-            )
-          
-        })}
-    
-        
+                <div className={style.SectionIconAndText}>
+                  <div className={style.SectionIconAndbtn}>
+                    <div className={style.SectionImageIcon}>
+                      <img
+                        src={require(`../../img/${data.image}`)}
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          width: "100%",
+                          height: "100%",
+                        }}
+                      />
+                    </div>
 
-        
-    <div >
+                    <div className={style.SectionTitle}>
+                      <div className={style.Text}>
+                        <span>{data.title}</span>
+                      </div>
+                      <div className={style.Btn}>
+                        {" "}
+                        <button
+                          onClick={() => {
+                            handleClick(data.id);
+                          }}
+                        >
+                          Дэлгэрэнгүй
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
 
-    
+        <div></div>
+      </div>
     </div>
-    
-    </div>
+  );
+};
 
-          </div>
-  )
-}
-
-export default Home
+export default Home;
